a3f3ad61760d938543ba9d02504df833573fa6f0
z0xi <1013295538@qq.com>
2021-03-06 11:07:31 +0800

    /mandetail一人一档
    增加几个svg。

---
+++ src/views/manfile/mandetail.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="app-container">
+    <div v-if="user">
+      <el-row :gutter="20">
+
+        <el-col :span="6" :xs="24">
+          <user-card :user="user" />
+        </el-col>
+
+        <el-col :span="18" :xs="24">
+          <el-card>
+            <el-tabs v-model="activeTab">
+              <el-tab-pane label="抓拍记录" name="activity">
+                <activity />
+              </el-tab-pane>
+              <el-tab-pane label="重点同行人分析" name="timeline">
+                <timeline />
+              </el-tab-pane>
+              <el-tab-pane label="历史预警" name="account">
+                <account :user="user" />
+              </el-tab-pane>
+            </el-tabs>
+          </el-card>
+        </el-col>
+
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import UserCard from './components/UserCard'
+import Activity from './components/Activity'
+import Timeline from './components/Timeline'
+import Account from './components/Account'
+
+export default {
+  name: 'mandetail',
+  components: { UserCard, Activity, Timeline, Account },
+  data() {
+    return {
+      user: {},
+      activeTab: 'activity'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name',
+      'avatar',
+    ])
+  },
+  created() {
+    this.getUser()
+  },
+  methods: {
+    getUser() {
+      this.user = {
+        name: this.name,
+        avatar: this.avatar
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/taskmanager/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="table-container">
+    <el-table :data="list" class="eltable"  style="width: 100%;padding-top: 15px;">
+      <el-table-column label="任务" min-width="200">
+        <template slot-scope="scope">
+          {{ scope.row.order_no | orderNoFilter }}
+        </template>
+      </el-table-column>
+      <el-table-column label="任务下达者" width="195" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.price }}
+        </template>
+      </el-table-column>
+      <el-table-column label="任务进行情况" width="150" align="center">
+        <template slot-scope="{row}">
+          <el-tag :type="row.status | statusFilter">
+            {{ row.status }}
+          </el-tag>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import { transactionList } from '@/api/remote-search'
+
+export default {
+  name:'taskmanager',
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        已完成: 'success',
+        进行中: 'danger'
+      }
+      return statusMap[status]
+    },
+    orderNoFilter(str) {
+      return str.substring(0, 30)
+    }
+  },
+  data() {
+    return {
+      list: null
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      transactionList().then(response => {
+        this.list = response.data.items.slice(0, 8)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.table-container{
+  padding: 32px;
+  position: relative;
+}
+
+/*最外层透明*/
+/deep/ .el-table, /deep/ .el-table__expanded-cell {
+  background-color: transparent;
+  color: white;
+}
+
+/* 表格内背景颜色 */
+/deep/ .el-table th,
+/deep/ .el-table tr,
+/deep/ .el-table td {
+  background-color: transparent;
+  color: white;
+}
+
+/*table item鼠标悬停颜色*/
+/deep/ .el-table tbody tr:hover > td {
+  background-color: #C0C4CC
+}
+
+</style>
