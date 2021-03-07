971b9abde13ab90d2ff8bc2534225065a3c059f9
Linyifan <linyifan19970218@163.com>
2021-03-07 17:46:29 +0800

    修改了个人信息页面

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,12 +1,44 @@
 <template>
-  <a>{{ user.name }}</a>
-</template>
-<script>
+  <div class="app-container">
+    <div v-if="user">
+      <el-row :gutter="20">
 
+        <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+          <box-card />
+        </el-col>
+
+        <el-col :span="18" :xs="24">
+          <el-card>
+            <el-tabs v-model="activeTab">
+              <el-tab-pane label="信息" name="activity">
+                <activity />
+              </el-tab-pane>
+              <el-tab-pane label="历史纪录" name="timeline">
+                <timeline />
+              </el-tab-pane>
+              <el-tab-pane label="账户信息" name="account">
+                <account :user="user" />
+              </el-tab-pane>
+            </el-tabs>
+          </el-card>
+        </el-col>
+
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
 import { mapGetters } from 'vuex'
+
+import Activity from './components/Activity'
+import Timeline from './components/Timeline'
+import Account from './components/Account'
+import BoxCard from './components/BoxCard'
 
 export default {
   name: 'Profile',
+  components: { BoxCard, Activity, Timeline, Account },
   data() {
     return {
       user: {},
@@ -27,6 +59,7 @@ export default {
     getUser() {
       this.user = {
         name: this.name,
+        role: this.roles.join(' | '),
         email: 'admin@test.com',
         avatar: this.avatar
       }
@@ -34,7 +67,10 @@ export default {
   }
 }
 </script>
-
-<style scoped>
+<style>
+.el-card{
+  background: transparent;
+  color:white;
+}
 
 </style>
