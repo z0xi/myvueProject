c165c456d70bc812b23df42700cdce0e47c25e17
Linyifan <linyifan19970218@163.com>
2021-03-12 20:36:00 +0800

    一人一档档案汇总，使用了v-for界面

--- src/views/document/summary/components/realnameDoc.vue
+++ src/views/document/summary/components/realnameDoc.vue
@@ -1,13 +1,62 @@
 <template>
-
+  <div class="counting">
+    <p>
+      共1007个档案
+    </p>
+    <el-row v-for="item in 2" :key="item">
+      <el-col :span="3" v-for="(o, index) in 6" :key="o" :offset="index > 0 ? 1 : 0">
+        <el-card shadow=“hover” :body-style="{ padding: '10px'}">
+          <img src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" class="image">
+          <div style="padding: 8px;">
+            <span>姓名</span>
+            <p>身份证号</p>
+            <div class="bottom">
+              <el-button type="text" class="button">操作按钮</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "realnameDoc"
+  date(){
+    return{
+
+    }
+
+  },
+  name:'realnameDoc'
 }
 </script>
 
 <style scoped>
+.counting{
+  color:white;
+}
+.bottom {
+  margin-top: 13px;
+  line-height: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  padding: 0;
+  min-height: auto;
+}
+
+.image {
+  width: 95%;
+  display: block;
+}
+.el-row {
+  margin-top: 40px;
+  margin-bottom: 4px;
+
+}
 
 </style>
--- src/views/document/summary/components/unrealnameDoc.vue
+++ src/views/document/summary/components/unrealnameDoc.vue
@@ -1,13 +1,62 @@
 <template>
-
+  <div class="counting">
+    <p>
+      共1007个档案
+    </p>
+    <el-row v-for="item in 2" :key="item">
+      <el-col :span="3" v-for="(o, index) in 6" :key="o" :offset="index > 0 ? 1 : 0">
+        <el-card shadow=“hover” :body-style="{ padding: '10px'}">
+          <img src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" class="image">
+          <div style="padding: 8px;">
+            <span>姓名</span>
+            <p>身份证号</p>
+            <div class="bottom">
+              <el-button type="text" class="button">操作按钮</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
-name: "unrealnameDoc"
+  date(){
+    return{
+
+    }
+
+  },
+  name:'unrealnameDoc'
 }
 </script>
 
 <style scoped>
+.counting{
+  color:white;
+}
+.bottom {
+  margin-top: 13px;
+  line-height: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  padding: 0;
+  min-height: auto;
+}
+
+.image {
+  width: 95%;
+  display: block;
+}
+.el-row {
+  margin-top: 40px;
+  margin-bottom: 4px;
+
+}
 
 </style>
